dfa861fdab2c9d076c3ca76372244a0e40d21299
nadian-melhem <60795209+nadian-melhem@users.noreply.github.com>
2023-11-14 23:41:49 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsBuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+module.exports = defineConfig({
+  e2e: {
+    video: true,
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsBuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+      return config;
+    },
+    specPattern: "cypress/e2e/*.feature",
+  },
+});
